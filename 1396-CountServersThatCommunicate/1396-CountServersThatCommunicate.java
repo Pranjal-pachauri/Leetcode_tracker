1f836e403ff9c3aaaee84827f4a4770dfbef33ce
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:05:12 +0530

    Create 1396-CountServersThatCommunicate

---
+++ 1396-CountServersThatCommunicate/1396-CountServersThatCommunicate.java
@@ -0,0 +1,26 @@
+// Last updated: 7/13/2026, 10:05:21 AM
+class Solution {
+    public int countServers(int[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+        int[] rc = new int[r];
+        int[] cc = new int[c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) {
+                    rc[i]++;
+                    cc[j]++;
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1 && (rc[i] > 1 || cc[j] > 1)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
