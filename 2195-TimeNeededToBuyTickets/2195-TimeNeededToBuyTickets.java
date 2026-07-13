1d73719500f1f7ba2f0fd614b1ca3299934eadc7
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:03:05 +0530

    Create 2195-TimeNeededToBuyTickets

---
+++ 2195-TimeNeededToBuyTickets/2195-TimeNeededToBuyTickets.java
@@ -0,0 +1,11 @@
+// Last updated: 7/13/2026, 10:03:14 AM
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+    int time=0;
+    for(int i=0;i<tickets.length;i++){
+        if(i<=k) time+=Math.min(tickets[k],tickets[i]);
+        else time+=Math.min(tickets[k]-1,tickets[i]);
+    }
+        
+   return time; }
+}
