27807f81ed2327d9a0886b265d2d846c7ba27409
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:02:57 +0530

    Create 2221-CheckIfAParenthesesStringCanBeValid

---
+++ 2221-CheckIfAParenthesesStringCanBeValid/2221-CheckIfAParenthesesStringCanBeValid.java
@@ -0,0 +1,29 @@
+// Last updated: 7/13/2026, 10:03:06 AM
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        if (s.length() % 2 != 0) 
+            return false;
+
+        int openCount = 0, closeCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (locked.charAt(i) == '0' || s.charAt(i) == '(') 
+                openCount++;
+            else 
+                openCount--;
+            if (openCount < 0) 
+                return false;
+        }
+
+        openCount = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (locked.charAt(i) == '0' || s.charAt(i) == ')') 
+                closeCount++;
+            else 
+                closeCount--;
+            if (closeCount < 0) 
+                return false;
+        }
+
+        return true;
+    }
+}
