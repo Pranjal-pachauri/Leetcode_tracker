d96e5d9f0e5e891aa10d58cb397e60eb43ad1ffb
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:02:13 +0530

    Create 2478-LongestNiceSubarray

---
+++ 2478-LongestNiceSubarray/2478-LongestNiceSubarray.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2026, 10:02:22 AM
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int bit = 0;
+        int max = 1;
+        int s = 0;
+        int e = 0;
+        int n = nums.length;
+        while(e < n){
+            while( (bit& nums[e]) != 0){
+                bit^=nums[s++];
+            }
+            bit|=nums[e];
+            if(e-s+1 > max){
+                max = e-s+1;
+            }
+            e++;
+        }
+        return max;
+    }
+}
