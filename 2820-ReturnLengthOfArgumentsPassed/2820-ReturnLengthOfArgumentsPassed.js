caa5705f9a7b9ec53c46caee5862a03ca3ea7461
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:01:13 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,13 @@
+// Last updated: 7/13/2026, 10:01:22 AM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
