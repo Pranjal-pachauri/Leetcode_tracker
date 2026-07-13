4002eeb8e00d847809861c7bb508a07d1f202e15
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:02:02 +0530

    Create 2533-BitwiseXorOfAllPairings

---
+++ 2533-BitwiseXorOfAllPairings/2533-BitwiseXorOfAllPairings.java
@@ -0,0 +1,16 @@
+// Last updated: 7/13/2026, 10:02:11 AM
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int xor1 = 0 , xor2 = 0;
+
+        if(m%2==1)
+            for(int i: nums1)
+                xor1 = xor1 ^ i;
+        if(n%2==1)
+            for(int i: nums2)
+                xor2 = xor2 ^ i;
+        return xor1 ^ xor2;
+    }
+}
