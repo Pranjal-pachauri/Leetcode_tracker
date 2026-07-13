e2f96dc79b8d0582f268fd098d77f5df331fe99c
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:08:33 +0530

    Create 547-NumberOfProvinces

---
+++ 547-NumberOfProvinces/547-NumberOfProvinces.java
@@ -0,0 +1,25 @@
+// Last updated: 7/13/2026, 10:08:42 AM
+class Solution {
+    public int findCircleNum(int[][] grh) {
+        int n = grh.length;
+        boolean[] visited = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                q.add(i);
+                while (q.size() != 0) {
+                    int node = q.remove();
+                    for (int j = 0; j < n; j++) {
+                        if (grh[node][j] == 1 && !visited[j]) {
+                            q.add(j);
+                            visited[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
