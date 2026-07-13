ec7401208be46cd73215a8bc04972dac2e9477e7
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:12:24 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2026, 10:12:33 AM
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int candy[]= new int[n];
+        Arrays.fill(candy,1);
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]) candy[i]=candy[i-1]+1;
+        }
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]) {
+                candy[i]=Math.max(candy[i+1]+1,candy[i]);
+        }}
+        int c=0;
+        for(int i:candy){
+            c+=i;
+        }
+
+   return c; 
+}}
