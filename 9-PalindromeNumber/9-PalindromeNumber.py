d228c3c7c67ae82a70836a1128b7b4edbc08baf4
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:16:59 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,8 @@
+# Last updated: 7/13/2026, 10:17:08 AM
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        if x == x[::-1]:
+            return True
+        else:
+            return False
