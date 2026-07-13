246242b157a935bdb2d24d1bd9e63dcb5b0bd42a
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:09:38 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -0,0 +1,21 @@
+// Last updated: 7/13/2026, 10:09:47 AM
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int e:nums){
+            map.put(e,map.getOrDefault(e,0)+1);
+        }
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list,(a,b)->map.get(b)-map.get(a));
+
+        int[] arr = new int[k];
+        for(int i=0;i<list.size();i++){
+            if(k>0){
+              arr[i]=list.get(i);
+            }
+            k--;
+        }
+        return arr;
+    }
+}
