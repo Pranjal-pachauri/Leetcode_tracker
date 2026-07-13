938b4dafe4dc0e373331807ee68c93fb6d6d27fe
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:09:14 +0530

    Create 401-BinaryWatch

---
+++ 401-BinaryWatch/401-BinaryWatch.java
@@ -0,0 +1,14 @@
+// Last updated: 7/13/2026, 10:09:23 AM
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> arr=new ArrayList<>();
+        for(int i=0;i<12;i++){
+            for(int j=0;j<60;j++){
+                if(Integer.bitCount(i)+Integer.bitCount(j)==turnedOn){
+                    arr.add(String.format("%d:%02d",i,j));
+                }
+            }
+        }
+        return arr;
+    }
+}
