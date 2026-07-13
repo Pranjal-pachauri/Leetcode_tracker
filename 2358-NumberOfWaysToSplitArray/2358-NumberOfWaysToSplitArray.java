1d214582043034b79906a9e4ad0af42d2282a330
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:02:35 +0530

    Create 2358-NumberOfWaysToSplitArray

---
+++ 2358-NumberOfWaysToSplitArray/2358-NumberOfWaysToSplitArray.java
@@ -0,0 +1,18 @@
+// Last updated: 7/13/2026, 10:02:44 AM
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long total=0,sum=0;
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            total+=nums[i];
+        }
+        for(int i=0;i<nums.length-1;i++){
+            sum+=nums[i];
+            if(sum>=total-sum){
+                c++;
+            }
+
+        }    
+        return c;
+        }
+}
