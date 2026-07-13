4beea60d7a0719cecd47c91df3ad062e5a335427
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:11:02 +0530

    Create 216-CombinationSumIii

---
+++ 216-CombinationSumIii/216-CombinationSumIii.java
@@ -0,0 +1,58 @@
+// Last updated: 7/13/2026, 10:11:11 AM
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        find(k, 1, n, new ArrayList<>(), ans);
+        return ans; 
+    }
+
+    public static void find(int k, int num, int target, ArrayList<Integer> lst, List<List<Integer>> ans) {
+      
+        if (target == 0 && k == 0) {
+            ans.add(new ArrayList<>(lst));
+            return;
+        }
+
+        for (int i = num; i < 10; i++) {
+            if (i > target || k <= 0) break; 
+            lst.add(i); 
+            find(k - 1, i + 1, target - i, lst, ans); 
+            lst.remove(lst.size() - 1);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
