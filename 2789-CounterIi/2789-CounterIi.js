63487e7ea8969f611d7e81a161005c6c89fdf04d
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:01:24 +0530

    Create 2789-CounterIi

---
+++ 2789-CounterIi/2789-CounterIi.js
@@ -0,0 +1,29 @@
+// Last updated: 7/13/2026, 10:01:33 AM
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let c=init;
+    return {
+        increment: function(){
+            c++;
+            return c;
+        }, decrement: function(){
+            c--;
+            return c;
+
+        }, reset: function(){
+            c=init;
+            return c;
+        }
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
