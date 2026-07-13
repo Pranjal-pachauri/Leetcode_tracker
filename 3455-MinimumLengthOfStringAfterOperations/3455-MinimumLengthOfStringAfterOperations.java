f22d606385aede657858d4160e11cbe36043d427
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:00:28 +0530

    Create 3455-MinimumLengthOfStringAfterOperations

---
+++ 3455-MinimumLengthOfStringAfterOperations/3455-MinimumLengthOfStringAfterOperations.java
@@ -0,0 +1,19 @@
+// Last updated: 7/13/2026, 10:00:37 AM
+class Solution {
+    public int minimumLength(String s) {
+        int[] charFrequency = new int[26];
+        int totalLength = 0;
+        for (char c : s.toCharArray()) {
+            charFrequency[c - 'a']++;
+        }
+        for (int frequency : charFrequency) {
+            if (frequency == 0) continue;
+            if (frequency % 2 == 0) {
+                totalLength += 2;
+            } else {
+                totalLength += 1;
+            }
+        }
+        return totalLength;
+    }
+}
