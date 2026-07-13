e7f1f1f35d4bb8d981b7c3d27e71a3614f674dff
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:00:41 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,11 @@
+// Last updated: 7/13/2026, 10:00:50 AM
+class Solution {
+    public int scoreOfString(String s) {
+       int ans = 0;
+
+        for(int i = 0; i < s.length()-1; i++) {
+            ans += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return ans;   
+    }
+}
