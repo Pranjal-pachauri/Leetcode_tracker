ab9281658cae4f28b9880980ad81b3bfd404f740
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 10:02:42 +0530

    Create 2308-DivideArrayIntoEqualPairs

---
+++ 2308-DivideArrayIntoEqualPairs/2308-DivideArrayIntoEqualPairs.java
@@ -0,0 +1,13 @@
+// Last updated: 7/13/2026, 10:02:51 AM
+class Solution {
+    public boolean divideArray(int[] nums) {
+        int[] freq=new int[501];
+        for(int num:nums){
+            freq[num]++;
+        }
+        for(int num:freq){
+            if(num % 2 == 1)return false;
+        }
+        return true;
+    }
+}
