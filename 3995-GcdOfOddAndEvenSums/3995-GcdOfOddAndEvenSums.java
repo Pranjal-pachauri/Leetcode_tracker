0ef63589965f3c246fc0e193deb05f6e40485ec3
Pranjal Pachauri <162669440+Pranjal-pachauri@users.noreply.github.com>
2026-07-13 09:59:52 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ 3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.java
@@ -0,0 +1,24 @@
+// Last updated: 7/13/2026, 10:00:01 AM
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+//         int s1=0,s2=0;
+//         int i=1;
+//         while(i<n*2){
+//             if(i%2==0) s2+=i;
+//             else s1+=i;
+//             i++;
+//         }
+//         int q=gcd(s1,s2);
+        
+//      return q;}
+//     public int gcd(int s1, int s2){
+//         int g=1;
+//         for(int i=1;i<=Math.min(s1,s2);i++){
+//           if (s1 % i == 0 && s2 % i == 0)
+// {
+//                 g=i;
+//             }}
+//        return g;
+    return n;
+    }
+}
